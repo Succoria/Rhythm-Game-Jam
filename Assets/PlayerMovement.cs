cd07dd7c453a78c48acee1e4eae717c264e35c72
Succoria <leisher.josh@gmail.com>
2022-06-25 14:36:56 +0200

    Movement script added

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private BeatManager beatManager;
+    private Vector3 target;
+    public KeyCode playerButton;
     // Start is called before the first frame update
     void Start()
     {
-        
+        beatManager = FindObjectOfType<BeatManager>();
+        target = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(playerButton))
+        {
+            target += Vector3.right;
+        }
         
+        transform.position = Vector3.MoveTowards(transform.position,target, 4 * Time.deltaTime);
     }
 }
